0533f2623a8ef5b5bbc82285d04c0aec66b272d0
Rohan Srivastwa <47479220+Rohan3003@users.noreply.github.com>
2023-06-01 07:33:42 +0530

    adding new files

---
+++ logging.py
@@ -0,0 +1,14 @@
+import logging
+
+def main() -> None:
+    
+    logging.basicConfig(filename ='test_log.log',level=logging.DEBUG)
+
+    logging.debug("This is a debug message for logging")
+    logging.info("This is an info message for logging")
+    logging.warning("This is a warning message for logging")
+    logging.error("This is an error message for logging")
+    logging.critical("This is a critical message for logging")
+
+if __name__ == "__main__":
+    main()
--- test.py
+++ test.py
@@ -3,6 +3,8 @@ from datetime import datetime
 
 @dataclass
 class Customer:
+    """_summary_
+    """
     name : str
     phone : str
     cc_number : str
@@ -10,7 +12,10 @@ class Customer:
     cc_exp_year : int
     cc_valid : bool = False
 
-#def validate_card(customer: Customer) -> bool:
+# def validate_card(customer: Customer) -> bool:
 
 
 type(Customer)
+
+# ! Critical issue 
+# 
