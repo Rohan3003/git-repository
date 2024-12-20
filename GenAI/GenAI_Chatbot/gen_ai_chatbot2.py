7c56528372dc7dc85d9dc280979c3ee85e0b5439
Rohan Srivastwa <rohan.srivastwa00@gmail.com>
2024-12-20 10:53:59 +0530

    secret key removed

--- GenAI/GenAI_Chatbot/gen_ai_chatbot2.py
+++ GenAI/GenAI_Chatbot/gen_ai_chatbot2.py
@@ -9,9 +9,6 @@ from langchain.chains import ConversationChain
 from langchain.memory import ConversationBufferMemory
 from langchain.llms import OpenAI
 
-open_ai_key = "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"
-#os.environ["OPENAI_API_KEY"] = open_ai_key
-
 llm = OpenAI(model="gpt-3.5-turbo", openai_api_key=open_ai_key)
 
 memory = ConversationBufferMemory()
